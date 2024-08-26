fc4a42d55df50fd12b97495c6f144c59817ea47f
anshuforu <anshu_foru@yahoo.co.in>
2024-08-26 19:25:17 +0530

    Update Eureka_Forbes_Twitter_Post_Sentiment_Analysis_Twitter_API.py

--- SentimentAnalysisCode/Eureka_Forbes_Twitter_Post_Sentiment_Analysis_Twitter_API.py
+++ SentimentAnalysisCode/Eureka_Forbes_Twitter_Post_Sentiment_Analysis_Twitter_API.py
@@ -1,11 +1,10 @@
 import tweepy
 
 # Twitter API credentials
-#Client_Secret= 3lgOnTmnKBPArpxBGPiptF9yglvPAmGbTXaQprqXkwUchPWt1V
-consumer_key = 'K2kbBKsnXwA879UFn7kvYYfxS'
-consumer_secret = 'VZZ6Ur9Dru5FqBgxXarZtr1KjKAe4NprVT7iL9p6o23rNL08f4'
-access_token = '372092129-MLpE1ZoM4dDbpdj0fU28gbgqanlfuQOqY0ICKODa'
-access_token_secret = 'HgWWhubrUoApcqvnwCTBWmWwKLcscVh6FyLXW6URO1yxH'
+consumer_key = 'sdhdshdfhdfhdfh'
+consumer_secret = 'hdfhdfhdfhdfhdfh'
+access_token = '4646436466-fhfghhd'
+access_token_secret = 'dfghdfhdfhdfhdfhdfh'
 
 # Set up Tweepy API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
